9160b27e3655b597dd42fe02183bce952e73f427
Kevin Ward <antiomiae@gmail.com>
2015-05-08 15:52:32 -0500

    Migrated to babel.

--- app/main.js
+++ app/main.js
@@ -1,32 +1,48 @@
-import THREE from 'static/lib/threejs/build/three';
+/*jshint esnext: true */
+//import 'static/lib/helvetiker_regular.typeface/index';
 
 // Set up the THREE context
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = 20;
+
+//import THREE from 'static/lib/threejs/build/three';
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+	color: 0xFFFFFF
+});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-var textGeometry = new THREE.TextGeometry("_castle_", { size: window.innerHeight / 5 });
-//var text = new THREE.Mesh(textGeometry, material);
-//scene.add(text);
-
-camera.position.z = 5;
+var textGeometry = new THREE.TextGeometry("_castle_ experiment", {
+	size: 1,
+	height: 0.05,
+	bevelThickness: 0.1,
+	bevelSize: 0.1,
+	bevelEnabled: false,
+	material: 0,
+	extrudeMaterial: 1
+});
+var textMaterial = new THREE.MeshBasicMaterial({
+	color: 0xeecc88
+});
+var text = new THREE.Mesh(textGeometry, textMaterial);
+scene.add(text);
 
 function render() {
 	requestAnimationFrame(render);
-	
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-	
-	renderer.render( scene, camera );
+
+	cube.rotation.x += 0.005;
+	cube.rotation.y += 0.001;
+
+	renderer.render(scene, camera);
 }
 render();
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,31 @@
 /*global require: true */
 
-var gulp       = require('gulp');
-var traceur    = require('gulp-traceur');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
 var sourceMaps = require('gulp-sourcemaps');
-var concat     = require('gulp-concat');
-var runSeq     = require('run-sequence');
-var del        = require('del');
-var connect    = require('gulp-connect');
-var bower      = require('gulp-bower');
+var concat = require('gulp-concat');
+var runSeq = require('run-sequence');
+var del = require('del');
+var connect = require('gulp-connect');
+var bower = require('gulp-bower');
 var wiredep = require('wiredep').stream;
 
 var dir = {
-  index      : './app/index.html',
-  sourceDir  : './app',
-  source     : ['./app/**/*.js', '!./app/{lib,lib/**}'],
-  main       : './app/main.js',
-  lib        : ['!app/lib/threejs/examples/**',
-                '!*.json',
-                './app/lib/**/*.*',
-                './node_modules/systemjs/dist/system.*',
-                './node_modules/es6-module-loader/dist/es6-module-loader.*'],
-  libOut     : './build/static/lib',
-  build      : './build',
-  targetAppJs: 'app.js',
-  img        : './app/assets/img/**/*.png',
-  imgOut     : './build/static/img'
+	index: './app/index.html',
+	sourceDir: './app',
+	source: ['./app/**/*.js', '!./app/{lib,lib/**}'],
+	main: './app/main.js',
+	lib: ['!*.json',
+		'./app/lib/**/*.*',
+		//'./node_modules/systemjs/dist/system.*',
+		//'./node_modules/es6-module-loader/dist/es6-module-loader.*',
+        'node_modules/es6-micro-loader/dist/system-polyfill.js'
+	],
+	libOut: './build/static/lib',
+	build: './build',
+	targetAppJs: 'app.js',
+	img: './app/assets/img/**/*.png',
+	imgOut: './build/static/img'
 };
 
 var staticCopyTasks = [];
@@ -35,68 +36,58 @@ copyTask('img');
 copyTask('lib');
 
 gulp.task('clean-build', function(cb) {
-  del(dir.build, cb);
+	del(dir.build, cb);
 });
 
 gulp.task('copy-static', staticCopyTasks);
 
-gulp.task('traceur-runtimes', function() {
-  gulp.src([traceur.RUNTIME_PATH])
-      .pipe(gulp.dest(dir.libOut));
-});
-
 gulp.task('compileApp', function() {
-  var traceurOptions = {
-    experimental          : true,
-    annotations           : true,
-    exponentiation        : true,
-    asyncFunctions        : true,
-    generatorComprehension: true,
-    arrayComprehension    : true,
-    memberVariables       : true,
-    symbols               : true,
-    types                 : true,
-    modules               : 'instantiate'
-  };
+	var babelOptions = {
+		modules: 'system'
+	};
 
-  return gulp.src(dir.source, {base:dir.sourceDir})
-    .pipe(sourceMaps.init())
-    .pipe(traceur(traceurOptions))
-    .pipe(sourceMaps.write('.'))
-    .pipe(gulp.dest(dir.build));
+
+	return gulp.src(dir.source, {
+			base: dir.sourceDir
+		})
+		.pipe(sourceMaps.init())
+		.pipe(concat('all.js'))
+		.pipe(babel(babelOptions))
+		.pipe(sourceMaps.write('.', {sourceMappingURLPrefix: '/'}))
+        .pipe(gulp.dest(dir.build));
 });
 
 gulp.task('watch', function() {
-  gulp.watch([dir.source, dir.lib, dir.index], ['build-dev']);
+	gulp.watch([dir.source, dir.lib, dir.index, 'gulpfile.js'], ['build-dev']);
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    root: 'build',
-    livereload: false
-  });
-})
-
-gulp.task('build-dev', function() {
-  runSeq('clean-build', ['traceur-runtimes', 'copy-static', 'compileApp'], 'bower')
-})
-
-gulp.task('default', function() {
-  runSeq('build-dev', ['watch', 'connect']);
+	connect.server({
+		root: 'build',
+		livereload: false
+	});
 });
 
-gulp.task('bower', function () {
-  gulp.src(dir.build + '/index.html')
-    .pipe(wiredep({
-      cwd: '.',
-      ignorePath: /\.\.\/app\//,
-      fileTypes: {
-        html: {
-          replace: {
-            js: '<script src="static/{{filePath}}"></script>'
-          }
-        }
-      }
-    }))
-    .pipe(gulp.dest(dir.build));
+gulp.task('build-dev', function() {
+	runSeq('clean-build', ['copy-static', 'compileApp'], 'bower')
+});
+
+gulp.task('default', function() {
+	runSeq('build-dev', ['watch', 'connect']);
+});
+
+gulp.task('bower', function() {
+	gulp.src(dir.build + '/index.html')
+		.pipe(wiredep({
+			cwd: '.',
+			ignorePath: /\.\.\/app\//,
+			fileTypes: {
+				html: {
+					replace: {
+						js: '<script src="static/{{filePath}}"></script>'
+					}
+				}
+			}
+		}))
+		.pipe(gulp.dest(dir.build));
 });
